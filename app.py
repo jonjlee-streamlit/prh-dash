98dee71ad0b3e5207cbfb70be1c6d5ffd143315f
jonjlee-streamlit <104783508+jonjlee-streamlit@users.noreply.github.com>
2023-04-06 17:29:06 +0000

    Initial skeleton

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def run():
+    """Main streamlit app entry point"""
+    st.title("PRH Dashboard Content.")
+    st.write("PRH Dashboard Content.")
+
+
+st.set_page_config(page_title="PRH Dashboard", layout="wide")
+run()
