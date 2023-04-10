83068acf83c9a2836bc3454bb5f9dc589a6a5097
Jonathan Lee <jonjlee@gmail.com>
2023-04-10 15:36:23 -0700

    Add gauge chart demo

--- src/ui.py
+++ src/ui.py
@@ -4,6 +4,7 @@ sidebar for configuration options, main app content, etc.
 """
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 from . import data
 
 
@@ -45,34 +46,61 @@ def show_main_content(settings: dict, data: data.ProcessedData):
     st.title("Rehabilitation Services")
 
     tab_1, tab_2, tab_3, tab_4 = st.tabs(
-        ["KPI & Productivity", "FTE", "Data", "Calculations"]
+        ["KPI & Productivity", "FTE", "Data", "(Calculations)"]
     )
     s = data.stats
 
     with tab_1:
         st.header("KPIs")
-        kpi_1, kpi_2, _kpi_3 = st.columns(3)
-        kpi_1.metric(
+        col_1, col_2, col_3 = st.columns(3)
+        col_1.metric(
             "Revenue per Encounter",
             "$%s" % round(s["actual_revenue_per_volume"]),
             "%s%% from target" % s["variance_revenue_per_volume"],
         )
 
-        kpi_2.metric(
+        col_2.metric(
             "Expense per Encounter",
             "$%s" % round(s["actual_expense_per_volume"]),
             delta="%s%% from target" % s["variance_expense_per_volume"],
             delta_color="inverse",
         )
 
+        show_gauge = col_3.checkbox('Show Gauge Chart Demo')
+        if show_gauge:
+            layout = go.Layout(margin=go.layout.Margin(l=25, r=25, t=0))
+            fig = go.Figure(
+                go.Indicator(
+                    mode = "gauge+number+delta",
+                    value = 116,
+                    number = {'prefix': "$"},
+                    domain = {'x': [0, 1], 'y': [0, 1]},
+                    delta = {'reference': 87, 'valueformat': '.0%', 'relative': True},
+                    gauge = {
+                        # 'axis': {'range': [None, 500], 'tickwidth': 1, 'tickcolor': "darkblue"},
+                        # 'bar': {'color': "darkblue"},
+                        # 'bgcolor': "white",
+                        # 'borderwidth': 2,
+                        # 'bordercolor': "gray",
+                        # 'steps': [
+                        #     {'range': [0, 250], 'color': 'cyan'},
+                        #     {'range': [250, 400], 'color': 'royalblue'}],
+                        'threshold': {
+                            'line': {'color': "#1D201F", 'width': 4},
+                            'thickness': 0.75,
+                            'value': 87}}))
+            fig.update_layout(layout, height=225)
+            col_3.plotly_chart(fig, use_container_width=True, theme=None, height=100)
+            col_3.markdown("<style>.js-plotly-plot .plotly .svg-container { max-height: 100px }</style>", unsafe_allow_html=True)
+        
         st.header("Productivity")
-        prod_1, prod_2, prod_3 = st.columns(3)
-        prod_1.metric("Hours per Encounter", round(s["actual_hours_per_volume"], 2))
-        prod_2.metric("Target Hours per Encounter", s["target_hours_per_volume"])
-        prod_3.metric("FTE Variance", round(s["fte_variance"], 2))
+        col_1, col_2, col_3 = st.columns(3)
+        col_1.metric("Hours per Encounter", round(s["actual_hours_per_volume"], 2))
+        col_1.metric("Target Hours per Encounter", s["target_hours_per_volume"])
+        col_1.metric("FTE Variance", round(s["fte_variance"], 2))
 
         v = s["fte_variance_dollars"]
-        prod_2.markdown(
+        col_2.markdown(
             "<p style='font-size:14px;'>Dollar Impact</p>"
             + f"<p style='margin-top:-15px; font-size:2rem; color:{'rgb(255, 43, 43)' if v < 0 else 'rgb(9, 171, 59)'}'>{_format_finance(v)}</p>",
             unsafe_allow_html=True,
