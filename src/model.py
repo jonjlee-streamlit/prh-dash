deddf897675870001a66b88cbc417afd690250ce
Jonathan Lee <jonjlee@gmail.com>
2023-08-24 22:57:11 -0700

    Income statement table definition

--- src/model.py
+++ src/model.py
@@ -33,3 +33,17 @@ class BudgetedHoursPerVolume(Base):
     dept_wd_id = mapped_column(String(10), nullable=False)
     dept_name = mapped_column(String, nullable=True)
     budgeted_hours_per_volume = mapped_column(Float, nullable=False)
+
+class IncomeStmt(Base):
+    __tablename__ = "income_stmt"
+    id = mapped_column(Integer, primary_key=True)
+    month = mapped_column(String(7), nullable=False)
+    ledger_acct = mapped_column(String, nullable=False)
+    cost_center = mapped_column(String, nullable=True)
+    spend_category = mapped_column(String, nullable=True)
+    revenue_category = mapped_column(String, nullable=True)
+    actual = mapped_column(Float, nullable=False)
+    budget = mapped_column(Float, nullable=False)
+    actual_ytd = mapped_column(Float, nullable=False)
+    budget_ytd = mapped_column(Float, nullable=False)
+    
