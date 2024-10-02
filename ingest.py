0f2b6c0c5ee93634f7860f6b3ff89e6a3dd1b41d
Jonathan Lee <jonjlee@gmail.com>
2024-10-01 21:09:09 -0700

    Refactor ingest.py to accept input directory and output file name

--- ingest.py
+++ ingest.py
@@ -1,6 +1,7 @@
 import os
 import contextlib
 import logging
+import argparse
 import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
@@ -28,31 +29,38 @@ SHOW_SQL_IN_LOG = False
 # Temporary DB to ingest data into
 TMP_DB_FILE = "db-tmp.sqlite3"
 
-# Location of data files: <app root>/data/
-BASE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-
-# Historical volume data is in the STATS worksheet of the Dashboard Supporting Data spreadsheet
-VOLUMES_FILE = os.path.join(BASE_PATH, "Dashboard Supporting Data 2024 v3.xlsx")
+# Specific worksheet names containing data to ingest in the Dashboard Supporting Data spreadsheet
 VOLUMES_SHEET = "STATS"
 UOS_SHEET = "UOS"
 VOLUMES_BUDGET_SHEET = "Data"
 HRS_PER_VOLUME_SHEET = "Prod.MH UOS"
 CONTRACTED_HRS_SHEET = "ProdHrs"
 
-# The Natural Class subdir contains income statment in one Excel file per month, eg,
-# ./Natural Class/2022/(01) Jan 2022 Natural Class.xlsx
-INCOME_STMT_PATH = os.path.join(BASE_PATH, "Natural Class")
-
-# PayPeriod subdir contians productive / non-productive hours and FTE data per month. eg,
-#   ./PayPeriod/2023/PP#1 2023 Payroll_Productivity_by_Cost_Center.xlsx
-# In addition, historical data for 2022 PP#1-25, which includes the clinic network, is lumped together a separate file:
-#   ./PayPeriod/2023/PP#1-PP#25 Payroll Productivity.xlsx
+# The historical hours data returned by get_file_paths() as historical_hours_file contains data for 2022
 HISTORICAL_HOURS_YEAR = 2022
-HISTORICAL_HOURS_FILE = os.path.join(
-    BASE_PATH, "PayPeriod", "2022", "PP#1-PP#26 Payroll Productivity.xlsx"
-)
-HOURS_PATH = os.path.join(BASE_PATH, "PayPeriod")
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Ingest data into the database.")
+    parser.add_argument("data_dir", help="Path to the data directory")
+    parser.add_argument("-o", "--out", help="Output database file path")
+    return parser.parse_args()
+
+def get_file_paths(base_path):
+    # Historical volume data is ion the Dashboard Supporting Data spreadsheet
+    volumes_file = os.path.join(base_path, "Dashboard Supporting Data 2024 v3.xlsx")
+
+    # The Natural Class subdir contains income statment in one Excel file per month, eg,
+    # ./Natural Class/2022/(01) Jan 2022 Natural Class.xlsx
+    income_stmt_path = os.path.join(base_path, "Natural Class")
+
+    # PayPeriod subdir contians productive / non-productive hours and FTE data per month. eg,
+    #   ./PayPeriod/2023/PP#1 2023 Payroll_Productivity_by_Cost_Center.xlsx
+    # In addition, historical data for 2022 PP#1-25, which includes the clinic network, is lumped together a separate file:
+    #   ./PayPeriod/2023/PP#1-PP#25 Payroll Productivity.xlsx
+    historical_hours_file = os.path.join(base_path, "PayPeriod", "2022", "PP#1-PP#26 Payroll Productivity.xlsx")
+    hours_path = os.path.join(base_path, "PayPeriod")
+
+    return volumes_file, income_stmt_path, hours_path, historical_hours_file
 
 def sanity_check_data_dir(base_path, volumes_file, income_stmt_path, hours_path):
     """
@@ -94,28 +102,33 @@ def find_data_files(path, exclude=None):
 
     return sorted(ret)
 
+def main():
+    args = parse_arguments()
+    base_path = args.data_dir
+    output_db_file = args.out or DEFAULT_DB_FILE
 
-if __name__ == "__main__":
+    volumes_file, income_stmt_path, hours_path, historical_hours_file = get_file_paths(base_path)
+
     # Sanity check data directory expected location and files
-    if not sanity_check_data_dir(BASE_PATH, VOLUMES_FILE, INCOME_STMT_PATH, HOURS_PATH):
+    if not sanity_check_data_dir(base_path, volumes_file, income_stmt_path, hours_path):
         logging.error("ERROR: data directory error (see above). Terminating.")
         exit(1)
 
     # Get list of dynamic data files, ie data organized as one Excel workbook per month
-    income_stmt_files = find_data_files(INCOME_STMT_PATH)
-    hours_files = find_data_files(HOURS_PATH, exclude=[HISTORICAL_HOURS_FILE])
+    income_stmt_files = find_data_files(income_stmt_path)
+    hours_files = find_data_files(hours_path, exclude=[historical_hours_file])
     source_files = (
-        [VOLUMES_FILE, HISTORICAL_HOURS_FILE] + income_stmt_files + hours_files
+        [volumes_file, historical_hours_file] + income_stmt_files + hours_files
     )
     source_files_str = "\n  ".join(source_files)
     logging.info(f"Discovered source files:\n  {source_files_str}")
 
     # TODO: data verification
-    # - VOLUMES_FILE, List worksheet: verify same data as static_data.WDID_TO_DEPTNAME
+    # - volumes_file, List worksheet: verify same data as static_data.WDID_TO_DEPTNAME
     # - Each income statement sheet has Ledger Account cell, and data in columns A:Q
     # - hours and income data is present for the latest month we have volume data for
     # - fte, volumes should all be non-negative. Hours can be negative for adjustments
-    if not sanity.check_data_files(VOLUMES_FILE, income_stmt_files):
+    if not sanity.check_data_files(volumes_file, income_stmt_files):
         logging.error("ERROR: data files sanity check failed (see above).")
         exit(1)
 
@@ -127,17 +140,17 @@ if __name__ == "__main__":
     logging.info(f"Created tables in {TMP_DB_FILE}")
 
     # Extract and perform basic transformation of data from spreadsheets
-    volumes_df = parse.read_volume_and_uos_data(VOLUMES_FILE, VOLUMES_SHEET)
-    uos_df = parse.read_volume_and_uos_data(VOLUMES_FILE, UOS_SHEET)
+    volumes_df = parse.read_volume_and_uos_data(volumes_file, VOLUMES_SHEET)
+    uos_df = parse.read_volume_and_uos_data(volumes_file, UOS_SHEET)
     budget_df = parse.read_budget_data(
-        VOLUMES_FILE, VOLUMES_BUDGET_SHEET, HRS_PER_VOLUME_SHEET, UOS_SHEET
+        volumes_file, VOLUMES_BUDGET_SHEET, HRS_PER_VOLUME_SHEET, UOS_SHEET
     )
     contracted_hours_updated_month, contracted_hours_df = (
-        parse.read_contracted_hours_data(VOLUMES_FILE, CONTRACTED_HRS_SHEET)
+        parse.read_contracted_hours_data(volumes_file, CONTRACTED_HRS_SHEET)
     )
     income_stmt_df = parse.read_income_stmt_data(income_stmt_files)
     historical_hours_df = parse.read_historical_hours_and_fte_data(
-        HISTORICAL_HOURS_FILE, HISTORICAL_HOURS_YEAR
+        historical_hours_file, HISTORICAL_HOURS_YEAR
     )
     hours_by_pay_period_df = parse.read_hours_and_fte_data(hours_files)
     hours_by_pay_period_df = pd.concat([historical_hours_df, hours_by_pay_period_df])
@@ -167,4 +180,9 @@ if __name__ == "__main__":
 
     # Move new database in place
     db_engine.dispose()
-    os.replace(TMP_DB_FILE, DEFAULT_DB_FILE)
+    os.replace(TMP_DB_FILE, output_db_file)
+    logging.info(f"Output to {output_db_file}")
+
+
+if __name__ == "__main__":
+    main();
